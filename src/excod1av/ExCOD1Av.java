0f8ae6dbed9a57b4662f0bb08702a9401f4d2e57
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2017-11-17 09:09:18 +0100

    Commit A:
    - Añadimos un print que nos pregunta nuestro nombre.

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -16,6 +16,8 @@ public class ExCOD1Av {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Buenos días, ¿Cuál es  tu nombre?");
     }
     
 }
